e0a730b67e156583f76fedb8c9b07d467f4ba949
Josue Alcantara <josue.almo@gmail.com>
2022-04-17 23:22:45 +0200

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardFrom from './From.svg';
+import Back from './Back.svg';
 import './App.css';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Carousel variant="dark">
+          <Carousel.Item interval={1000}>
+            <img src={CardFrom} className="App-logo" alt="logo" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={Back} className="App-logo" alt="logo" />
+          </Carousel.Item>
+        </Carousel>
       </header>
+      
     </div>
   );
 }
